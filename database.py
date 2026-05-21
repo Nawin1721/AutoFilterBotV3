991597789c9fc0b2a22512c279ed6075560ca8cc
Naveen Kadali <naveenkadali1237@gmail.com>
2026-05-21 10:44:22 +0530

    Update database.py

--- database.py
+++ database.py
@@ -11,4 +11,20 @@ users_col = db["users"]
 
 requests_col = db["requests"]
 
+# =========================
+# DATABASE INDEXES
+# =========================
+
+files_col.create_index("file_name")
+
+files_col.create_index("search_text")
+
+files_col.create_index("caption")
+
+users_col.create_index("user_id")
+
+requests_col.create_index("user_id")
+
+
 print("MongoDB Connected ✅")
+print("Indexes Created ✅")
