e9d119dd7f8b038460e3574e65e1c874c9c8f57a
Naveen Kadali <naveenkadali1237@gmail.com>
2026-05-22 22:22:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from telegram.ext import Application
 
 from config import BOT_TOKEN
 
+from plugins.start import photo_handler
+
 from plugins.auto_index import auto_index
 from plugins.search import search_handler
 from plugins.callback import callback_handler
@@ -9,6 +11,27 @@ from plugins.broadcast import broadcast_handler
 from plugins.request import request_handler
 from plugins.stats import stats_handler
 from plugins.start import start_handler, help_handler
+
+from telegram.ext import MessageHandler, filters
+
+# =========================
+# GET PHOTO FILE ID
+# =========================
+async def get_photo_id(update, context):
+
+    if update.message.photo:
+
+        file_id = update.message.photo[-1].file_id
+
+        await update.message.reply_text(
+            f"{file_id}"
+        )
+
+photo_handler = MessageHandler(
+    filters.PHOTO,
+    get_photo_id
+)
+
 # from plugins.inline import inline_handler
 
 app = (
@@ -38,6 +61,7 @@ app.add_handler(stats_handler)
 # START & HELP
 app.add_handler(start_handler)
 app.add_handler(help_handler)
+app.add_handler(photo_handler)
 
 # INLINE SEARCH
 # app.add_handler(inline_handler)
