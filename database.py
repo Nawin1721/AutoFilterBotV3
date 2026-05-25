451208c096e4aa998b2d129211b4d600e2e69147
Naveen Kadali <naveenkadali1237@gmail.com>
2026-05-25 16:04:48 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,30 +1,44 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
+
 from config import MONGO_URL, DB_NAME
 
-client = MongoClient(MONGO_URL)
+
+# =========================
+# MONGO CLIENT
+# =========================
+
+client = AsyncIOMotorClient(MONGO_URL)
 
 db = client[DB_NAME]
 
-files_col = db["files"] 
+
+# =========================
+# COLLECTIONS
+# =========================
+
+files_col = db["files"]
 
 users_col = db["users"]
 
 requests_col = db["requests"]
 
+
 # =========================
-# DATABASE INDEXES
+# CREATE INDEXES
 # =========================
 
-files_col.create_index("file_name")
+async def create_indexes():
 
-files_col.create_index("search_text")
+    await files_col.create_index("file_name")
 
-files_col.create_index("caption")
+    await files_col.create_index("search_text")
 
-users_col.create_index("user_id")
+    await files_col.create_index("caption")
 
-requests_col.create_index("user_id")
+    await users_col.create_index("user_id")
 
+    await requests_col.create_index("user_id")
 
-print("MongoDB Connected ✅")
-print("Indexes Created ✅")
+    print("MongoDB Connected ✅")
+
+    print("Indexes Created ✅")
