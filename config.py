295a985cda7ddf0ce4b4f137c825d16a9dabeae7
Naveen Kadali <naveenkadali1237@gmail.com>
2026-05-27 13:22:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+DB_NAME = os.getenv("DB_NAME")
+
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+
+GROUP_ID = int(os.getenv("GROUP_ID"))
+
+DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID"))
+
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
