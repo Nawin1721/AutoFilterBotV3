0a8512bcd4bd9085a71ba3e16fdeb9aee48a3c14
Naveen Kadali <naveenkadali1237@gmail.com>
2026-05-29 17:17:41 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,44 +1,38 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from config import MONGO_URL, DB_NAME
+from config import (
+    MONGO_URL,
+    MONGO_URL_2,
+    DB_NAME,
+    DB_NAME_2
+)
 
+client1 = AsyncIOMotorClient(MONGO_URL)
+db1 = client1[DB_NAME]
 
-# =========================
-# MONGO CLIENT
-# =========================
+client2 = AsyncIOMotorClient(MONGO_URL_2)
+db2 = client2[DB_NAME_2]
 
-client = AsyncIOMotorClient(MONGO_URL)
+files_col_1 = db1["files"]
+files_col_2 = db2["files"]
 
-db = client[DB_NAME]
+users_col = db1["users"]
+requests_col = db1["requests"]
 
-
-# =========================
-# COLLECTIONS
-# =========================
-
-files_col = db["files"]
-
-users_col = db["users"]
-
-requests_col = db["requests"]
-
-
-# =========================
-# CREATE INDEXES
-# =========================
 
 async def create_indexes():
 
-    await files_col.create_index("file_name")
+    await files_col_1.create_index("file_name")
+    await files_col_1.create_index("search_text")
+    await files_col_1.create_index("caption")
 
-    await files_col.create_index("search_text")
-
-    await files_col.create_index("caption")
+    await files_col_2.create_index("file_name")
+    await files_col_2.create_index("search_text")
+    await files_col_2.create_index("caption")
 
     await users_col.create_index("user_id")
-
     await requests_col.create_index("user_id")
 
-    print("MongoDB Connected ✅")
-
+    print("MongoDB 1 Connected ✅")
+    print("MongoDB 2 Connected ✅")
     print("Indexes Created ✅")
