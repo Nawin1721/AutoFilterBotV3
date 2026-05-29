149beeea9678fdf7852874b2fd5b9388ca45ee98
Naveen Kadali <naveenkadali1237@gmail.com>
2026-05-29 17:22:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,16 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 MONGO_URL = os.getenv("MONGO_URL")
-
 MONGO_URL_2 = os.getenv("MONGO_URL_2")
 
 DB_NAME = os.getenv("DB_NAME")
+DB_NAME_2 = os.getenv("DB_NAME_2")
 
 BOT_USERNAME = os.getenv("BOT_USERNAME")
 
 GROUP_ID = int(os.getenv("GROUP_ID"))
 
 DB_CHANNEL_ID = int(os.getenv("DB_CHANNEL_ID"))
-
 DB_CHANNEL_ID_2 = int(os.getenv("DB_CHANNEL_ID_2"))
 
 FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL")
